257268c851b6b27d19c269c60f4872b63a858627
andrewgrabko <grabko290587@gmail.com>
2018-06-08 16:24:23 +0300

    fixed mistakes

--- js/main.js
+++ js/main.js
@@ -8,14 +8,39 @@
 			infinite: false,
 			arrow:false
 		});
-
+		
+		var $newSlider = $('.ba-news__slider');
+		$newSlider.slick({
+			dots: true,
+			infinite: false,
+			arrow:false,
+			slidesToShow:3,
+			slidesToScroll:1
+		});
 
 
 	});
 
-	
+	var baMap;
+
+	function initMap() {
+
+		var poltava = {lat: 49.568528, lng: 34.585432};
+
+		baMap = new google.maps.Map(
+			document.querySelector('.ba-map'), 
+			{
+				center: poltava,
+				zoom: 17,
+				disableDefaultUI:true
+			});
+	}
+
+ // initMap();
+window.addEventListener('load', initMap);
 
 
 
+ 
 })(jQuery);
 
